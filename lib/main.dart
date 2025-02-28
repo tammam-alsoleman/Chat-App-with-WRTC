f2f57cbb32b9ad66687ff7a1c5d1adc8fc38f5a1
tammam-alsoleman <tammamalsleman311@gmail.com>
2025-02-28 18:55:00 +0300

    forth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'webrtc_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(MyApp());
